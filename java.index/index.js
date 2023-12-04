247be6fcf4ab706df6fe102aff547a578f1e2d9b
unknown <Luthandophumza17@gmail.com>
2023-12-04 13:56:54 +0200

    bootstrap

---
+++ java.index/index.js
@@ -0,0 +1,33 @@
+var say = "Hello World";
+var num = 120
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+
+console.log(say)
+console.log (num)
+
+var say= "Hello world"
+var num = 120
+
+for(var x =0; x<=50; x++){
+    num -=5
+    console.log(say)
+}
